704d1e6ea557d0dff0d914e566b1888df72c0b93
Mehdi DIAH <mdiah@lerebours.org>
2020-10-15 12:41:22 +0200

    test de publi

--- TestDepotGithub/MainWindow.xaml.cs
+++ TestDepotGithub/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TestDepotGithub
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //test
+        }
     }
 }
